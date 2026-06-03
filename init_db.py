554756b5f3a6820ea8c561f18c2b58355a400251
zivzivadzeleri <zivzivadzeleri@gmail.com>
2026-06-03 08:16:07 +0400

    Refactor database connection to use relative path
    Updated database path to be relative to the script's location.

--- init_db.py
+++ init_db.py
@@ -1,6 +1,10 @@
 import sqlite3
+import os
 
-conn = sqlite3.connect(r'C:\Users\PC_LION\Desktop\bfi_app\database.db')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "database.db")
+
+conn = sqlite3.connect(DB_PATH)
 
 cursor = conn.cursor()
 
